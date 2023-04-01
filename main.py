ca43a142d2aa8133533fc29e291c94587c7b0e76
doshisahil <sahil000005@gmail.com>
2023-04-01 12:16:31 +0530

    Update main.py
    Fixed issues

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ import sqlite3
 import hashlib
 import tkinter as tk
 from tkinter import filedialog
-
+import threading
 
 class DiskCloneUI:
     def __init__(self):
         # Create the main window
         self.root = tk.Tk()
-        # self.root.geometry("500x200")
-        # self.root.overrideredirect(True)  # remove window decorations
         self.root.title('Disk Clone')
 
         # Create the source path entry and browse button
@@ -30,10 +28,17 @@ class DiskCloneUI:
 
         # Create the copy button
         self.copy_button = tk.Button(self.root, text='Copy', command=self.copy_hard_disk)
-        self.copy_button.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+        self.copy_button.grid(row=2, column=0, padx=5, pady=5)
+
+        self.cancel = False
+        self.cancel_button = tk.Button(self.root, text='Cancel', command=self.cancel_op)
+        self.cancel_button.grid(row=2, column=1, padx=5, pady=5)
 
         # Initialize the database path variable
         self.db_path = 'disk_clone.db'
+
+    def cancel_op(self):
+        self.cancel = True
 
     def browse_source_path(self):
         # Ask the user to select a source directory and update the source path entry
@@ -46,6 +51,10 @@ class DiskCloneUI:
         self.destination_path_var.set(destination_path)
 
     def copy_hard_disk(self):
+        # Create a new thread to perform the copy
+        threading.Thread(target=self.copy_files).start()
+
+    def copy_files(self):
         # Get the source and destination paths from the UI
         source_path = self.source_path_var.get()
         destination_path = self.destination_path_var.get()
@@ -59,11 +68,13 @@ class DiskCloneUI:
         # Walk through the source directory and copy each file to the destination directory
         for root, dirs, files in os.walk(source_path):
             for file in files:
+                if self.cancel:
+                    break
                 source_file_path = os.path.join(root, file)
                 destination_file_path = os.path.join(destination_path, os.path.relpath(source_file_path, source_path))
 
                 # Check if the file has already been copied
-                cursor.execute('SELECT path FROM files WHERE path = ?', (destination_file_path,))
+                cursor.execute('SELECT path FROM files WHERE path = ?and status = "OK"', (destination_file_path,))
                 existing_file = cursor.fetchone()
                 if existing_file:
                     # File has already been copied, skip it
@@ -71,14 +82,8 @@ class DiskCloneUI:
 
                 try:
                     # Copy the file
+                    os.makedirs(os.path.dirname(destination_file_path), exist_ok=True)
                     shutil.copy2(source_file_path, destination_file_path)
-                except Exception as e:
-                    # File copy failed, mark it as an error in the database
-                    cursor.execute('INSERT INTO files (path, md5, status) VALUES (?, ?, ?)',
-                                   (destination_file_path, '', 'Error: ' + str(e)))
-                    conn.commit()
-                    continue
-
                     # Compute the MD5 hash of the file
                     md5_hash = hashlib.md5()
                     with open(destination_file_path, 'rb') as f:
@@ -93,14 +98,17 @@ class DiskCloneUI:
                     cursor.execute('INSERT INTO files (path, md5, status) VALUES (?, ?, ?)',
                                    (destination_file_path, md5, 'OK'))
                     conn.commit()
+                except Exception as e:
+                    # File copy failed, mark it as an error in the database
+                    cursor.execute('INSERT INTO files (path, md5, status) VALUES (?, ?, ?)',
+                                   (destination_file_path, '', 'Error: ' + str(e)))
+                    conn.commit()
+                    continue
 
-                    # Close the database connection
-                    self.root.update()
+        conn.close()
 
-                conn.close()
-
-                # Validate the integrity of the copied files
-                self.validate_integrity()
+        # Validate the integrity of the copied files
+        self.validate_integrity()
 
     def validate_integrity(self):
         # Connect to the database
